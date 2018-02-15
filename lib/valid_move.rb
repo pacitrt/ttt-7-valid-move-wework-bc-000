998eaebd2373926d2ce958a97031dfb17a75c83b
Rio Pacit <rio.pacit@wework.com>
2018-02-15 21:35:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,6 +7,6 @@ def valid_move?(board, index)
   if index.between?(0,8) && !position_taken?(board, index)
     return board[index] = input
   else
-    return false
+    return nil or false
 end
 end
