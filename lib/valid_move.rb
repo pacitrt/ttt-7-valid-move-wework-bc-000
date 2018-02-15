cd8f70e37fb71d76c83d2cd9ab29a4f232ea75e6
Rio Pacit <rio.pacit@wework.com>
2018-02-15 21:37:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,8 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def valid_move?(board,index)
   if index.between?(0,8) && !position_taken?(board,index)
     return true
